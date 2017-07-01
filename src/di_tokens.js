46d5556cde0c3553b1bd8a65b8194e6635989132
Cédric Belin <cedric@belin.io>
2017-07-01 15:12:14 +0200

    Added the `COOKIE_OPTIONS` injection token

---
+++ src/di_tokens.js
@@ -0,0 +1,7 @@
+import {InjectionToken} from '@angular/core';
+
+/**
+ * An injection token representing the default cookie options.
+ * @type {InjectionToken}
+ */
+export const COOKIE_OPTIONS = new InjectionToken('COOKIE_OPTIONS');
