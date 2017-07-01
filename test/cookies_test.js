f02dc17295a12cdcb910d2f1276c5c9956e78415
Cédric Belin <cedric@belin.io>
2017-07-01 22:02:46 +0200

    Replaced the cookie mock by an external library

--- test/cookies_test.js
+++ test/cookies_test.js
@@ -1,44 +1,29 @@
 'use strict';
 
 import {expect} from 'chai';
-import {afterEach, beforeEach, describe, it} from 'mocha';
+import CookieMock from 'ec.cookie-mock';
+import {afterEach, describe, it} from 'mocha';
 import {CookieOptions, Cookies} from '../src/index';
 
 /**
  * @test {Cookies}
  */
 describe('Cookies', () => {
-  let backend = {
-    _value: {},
-    get cookie() {
-      return Object.entries(this._value).map(([key, value]) => `${key}=${value}`).join(';');
-    },
-    set cookie(value) {
-      let offset = value.indexOf('=');
-      let key = value.substr(0, offset);
-      this._value[key] = value.substr(offset + 1);
-    }
-  };
-
-  let options;
-  beforeEach('reset the cookie backend', () => {
-    backend._value = {};
-    options = new CookieOptions;
-  });
 
   /**
    * @test {Cookies#keys}
    */
   describe('#keys', () => {
     it('should return an empty array if the current document has no associated cookie', () => {
-      expect(new Cookies(options, backend).keys).to.be.an('array').that.is.empty;
+      expect(new Cookies(new CookieOptions, new CookieMock).keys).to.be.an('array').that.is.empty;
     });
 
     it('should return the keys of the cookies associated with the current document', () => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
 
-      let keys = new Cookies(options, backend).keys;
+      let keys = new Cookies(new CookieOptions, backend).keys;
       expect(keys).to.be.an('array').and.have.lengthOf(2);
       expect(keys[0]).to.equal('foo');
       expect(keys[1]).to.equal('bar');
@@ -50,13 +35,14 @@ describe('Cookies', () => {
    */
   describe('#length', () => {
     it('should return zero if the current document has no associated cookie', () => {
-      expect(new Cookies(options, backend)).to.have.lengthOf(0);
+      expect(new Cookies(new CookieOptions, new CookieMock)).to.have.lengthOf(0);
     });
 
     it('should return the number of cookies associated with the current document', () => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
-      expect(new Cookies(options, backend)).to.have.lengthOf(2);
+      expect(new Cookies(new CookieOptions, backend)).to.have.lengthOf(2);
     });
   });
 
@@ -70,7 +56,7 @@ describe('Cookies', () => {
     );
 
     it('should trigger an event when a cookie is added', done => {
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, new CookieMock);
       subscription = cookies.onChanges.subscribe(changes => {
         expect(changes).to.be.an('array').and.have.lengthOf(1);
 
@@ -87,9 +73,10 @@ describe('Cookies', () => {
     });
 
     it('should trigger an event when a cookie is updated', done => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       subscription = cookies.onChanges.subscribe(changes => {
         expect(changes).to.be.an('array').and.have.lengthOf(1);
 
@@ -106,9 +93,10 @@ describe('Cookies', () => {
     });
 
     it('should trigger an event when a cookie is removed', done => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       subscription = cookies.onChanges.subscribe(changes => {
         expect(changes).to.be.an('array').and.have.lengthOf(1);
 
@@ -125,10 +113,11 @@ describe('Cookies', () => {
     });
 
     it('should trigger an event when all the cookies are removed', done => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       subscription = cookies.onChanges.subscribe(changes => {
         expect(changes).to.be.an('array').and.have.lengthOf(2);
 
@@ -156,17 +145,19 @@ describe('Cookies', () => {
    */
   describe('#[Symbol.iterator]()', () => {
     it('should return a done iterator if the current document has no associated cookie', () => {
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, new CookieMock);
       let iterator = cookies[Symbol.iterator]();
       expect(iterator.next().done).to.be.true;
     });
 
     it('should return a value iterator if the current document has associated cookies', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
 
+      let cookies = new Cookies(new CookieOptions, backend);
       let iterator = cookies[Symbol.iterator]();
+
       let next = iterator.next();
       expect(next.done).to.be.false;
       expect(next.value).to.be.an('array');
@@ -186,13 +177,14 @@ describe('Cookies', () => {
    */
   describe('#clear()', () => {
     it('should remove all the cookies associated with the current document', () => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       cookies.clear();
-      expect(backend.cookie).to.contain('foo=; expires=Thu, 01 Jan 1970 00:00:00 GMT');
-      expect(backend.cookie).to.contain('bar=; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+      expect(backend.cookie).to.not.contain('foo');
+      expect(backend.cookie).to.not.contain('bar');
     });
   });
 
@@ -201,7 +193,9 @@ describe('Cookies', () => {
    */
   describe('#get()', () => {
     it('should properly get the cookies associated with the current document', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
+      let cookies = new Cookies(new CookieOptions, backend);
+
       expect(cookies.get('foo')).to.be.null;
       expect(cookies.get('foo', '123')).to.equal('123');
 
@@ -218,7 +212,9 @@ describe('Cookies', () => {
    */
   describe('#getObject()', () => {
     it('should properly get the deserialized cookies associated with the current document', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
+      let cookies = new Cookies(new CookieOptions, backend);
+
       expect(cookies.getObject('foo')).to.be.null;
       expect(cookies.getObject('foo', {key: 'value'})).to.deep.equal({key: 'value'});
 
@@ -234,8 +230,10 @@ describe('Cookies', () => {
     });
 
     it('should return the default value if the value can\'t be deserialized', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
+
+      let cookies = new Cookies(new CookieOptions, backend);
       expect(cookies.getObject('foo', 'defaultValue')).to.equal('defaultValue');
     });
   });
@@ -245,13 +243,14 @@ describe('Cookies', () => {
    */
   describe('#has()', () => {
     it('should return `false` if the current document has an associated cookie with the specified key', () => {
-      expect(new Cookies(options, backend).has('foo')).to.be.false;
+      expect(new Cookies(new CookieOptions, new CookieMock).has('foo')).to.be.false;
     });
 
     it('should return `true` if the current document does not have an associated cookie with the specified key', () => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       expect(cookies.has('foo')).to.be.true;
       expect(cookies.has('bar')).to.be.false;
     });
@@ -262,16 +261,17 @@ describe('Cookies', () => {
    */
   describe('#remove()', () => {
     it('should properly remove the cookies associated with the current document', () => {
+      let backend = new CookieMock;
       backend.cookie = 'foo=bar';
       backend.cookie = 'bar=baz';
 
-      let cookies = new Cookies(options, backend);
+      let cookies = new Cookies(new CookieOptions, backend);
       cookies.remove('foo');
-      expect(backend.cookie).to.contain('foo=; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+      expect(backend.cookie).to.not.contain('foo');
       expect(backend.cookie).to.contain('bar=baz');
 
       cookies.remove('bar');
-      expect(backend.cookie).to.contain('bar=; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+      expect(backend.cookie).to.not.contain('bar');
     });
   });
 
@@ -280,7 +280,8 @@ describe('Cookies', () => {
    */
   describe('#set()', () => {
     it('should properly set the cookies associated with the current document', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
+      let cookies = new Cookies(new CookieOptions, backend);
       expect(backend.cookie).to.not.contain('foo');
 
       cookies.set('foo', 'bar');
@@ -296,7 +297,8 @@ describe('Cookies', () => {
    */
   describe('#setObject()', () => {
     it('should properly serialize and set the cookies associated with the current document', () => {
-      let cookies = new Cookies(options, backend);
+      let backend = new CookieMock;
+      let cookies = new Cookies(new CookieOptions, backend);
       expect(backend.cookie).to.not.contain('foo');
 
       cookies.setObject('foo', 123);
