c06a0a6441703683c894c1823a6486a6d8ba76dd
Cédric Belin <cedric@belin.io>
2019-08-27 19:56:43 +0200

    Moved the MkDocs settings to the "doc" folder

--- gulpfile.ts
+++ gulpfile.ts
@@ -38,8 +38,8 @@ task('coverage', () => _exec('coveralls', ['var/lcov.info']));
 task('doc', async () => {
   for (const path of ['CHANGELOG.md', 'LICENSE.md']) await copyFile(path, `doc/about/${path.toLowerCase()}`);
   await _exec('compodoc', ['--config=etc/compodoc.json']);
-  await _exec('mkdocs', ['build', '--config-file=etc/mkdocs.yaml']);
-  return del(['doc/about/changelog.md', 'doc/about/license.md']);
+  await _exec('mkdocs', ['build', '--config-file=doc/mkdocs.yaml']);
+  return del(['doc/about/changelog.md', 'doc/about/license.md', 'web/mkdocs.yaml']);
 });
 
 /** Fixes the coding standards issues. */
