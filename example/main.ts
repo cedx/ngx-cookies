1d102ffcf584f7f2b70ef412cc3aaf17b0368a38
Cédric Belin <cedric@belin.io>
2019-09-03 16:33:07 +0200

    Updated the settings of the static analyzer

--- example/main.ts
+++ example/main.ts
@@ -1,4 +1,4 @@
-/* eslint-disable capitalized-comments, line-comment-position */
+/* eslint-disable capitalized-comments, line-comment-position, no-console */
 import {Component, NgModule, OnInit} from '@angular/core';
 import {Cookies, CookieOptions} from '@cedx/ngx-cookies';
 
